107e3017c45a5717b0232697e16e754f7dfcdc0c
wei wei <wei.wei9@zte.com.cn>
2016-08-05 08:37:04 +0800

    Add DataMod class

---
+++ src/com/loraclaa/DataMod.java
@@ -0,0 +1,40 @@
+package com.loraclaa;
+
+public class DataMod {
+
+	
+	private String content;
+	private String name;
+	private int length;
+	
+	public DataMod() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
+
+}
